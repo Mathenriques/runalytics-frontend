7557f3213b03eccd9c40d04873ec5212e37e1b0e
Mathenriques <math.marqui@gmail.com>
2024-05-27 16:30:05 -0300

    layout form create workout done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { AuthGuard } from './services/auth-guard.service';
 import { SignUpComponent } from './pages/signUp/signUp.component';
+import { WorkoutWeekFormComponent } from './pages/workout-week-form/workout-week-form.component';
 
 export const routes: Routes = [
   {
@@ -22,5 +23,10 @@ export const routes: Routes = [
     path: "user/profile/:id",
     component: UserProfileComponent,
     canActivate: [AuthGuard]
+  },
+  {
+    path: "user/definir-semana",
+    component: WorkoutWeekFormComponent,
+    // canActivate: [AuthGuard],
   }
 ];
---
+++ src/app/pages/workout-week-form/workout-week-form.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { Form, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { CheckboxSelectComponent } from '../../components/checkbox-select/checkbox-select.component';
+
+@Component({
+  selector: 'app-workout-week-form',
+  standalone: true,
+  imports: [ReactiveFormsModule, PrimaryInputComponent, CheckboxSelectComponent],
+  templateUrl: './workout-week-form.component.html',
+  styleUrl: './workout-week-form.component.scss'
+})
+export class WorkoutWeekFormComponent {
+
+  workoutWeekFrom: FormGroup;
+  isDisabled: boolean = false;
+
+  booleanOptions = [
+    { label: 'Sim', value: true },
+    { label: 'Não', value: false },
+  ]
+
+  sleepingOptions = [
+    { label: '1', value: 1 },
+    { label: '2', value: 2 },
+    { label: '3', value: 3 },
+    { label: '4', value: 4 },
+    { label: '5', value: 5 },
+    { label: '6', value: 6 },
+    { label: '7', value: 7 },
+    { label: '8', value: 8 },
+    { label: '9', value: 9 },
+    { label: '10', value: 10 },
+  ]
+  constructor() {
+    this.workoutWeekFrom = new FormGroup({
+      start_date: new FormControl(''),
+      end_date: new FormControl(''),
+      weekly_training_volume: new FormControl(''),
+      didConditioning: new FormControl(''),
+      conditioning_frequency: new FormControl(''),
+      sleep_hours: new FormControl(''),
+      stress_level: new FormControl(''),
+      didMyofascialRelease: new FormControl(''),
+      feelPain: new FormControl(''),
+      painLocal: new FormControl(''),
+    });
+  }
+}
