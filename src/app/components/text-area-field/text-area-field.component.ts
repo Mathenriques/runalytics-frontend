379568e3d8802c1e2cbde8cf59d597e830f2a23d
Mathenriques <math.marqui@gmail.com>
2024-06-14 09:53:53 -0300

    Terminando form

--- src/app/components/text-area-field/text-area-field.component.ts
+++ src/app/components/text-area-field/text-area-field.component.ts
@@ -1,10 +1,17 @@
-import { Component, Input } from '@angular/core';
-import { ControlValueAccessor } from '@angular/forms';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-text-area-field',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextAreaFieldComponent),
+      multi: true
+    }
+  ],
   templateUrl: './text-area-field.component.html',
   styleUrl: './text-area-field.component.scss'
 })
