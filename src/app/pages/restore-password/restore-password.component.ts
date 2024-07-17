153f3f07cf77c5df7f01c0a35516bc8803e63cc4
Matheus Robusti Henriques Marqui <CIANDT\mmarqui@lnb024261cps.ciandt.global>
2024-07-17 11:48:54 -0300

    Tela de recuperacao sem css

--- src/app/pages/restore-password/restore-password.component.ts
+++ src/app/pages/restore-password/restore-password.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 
 @Component({
   selector: 'app-restore-password',
   standalone: true,
-  imports: [],
+  imports: [PrimaryInputComponent],
   templateUrl: './restore-password.component.html',
   styleUrl: './restore-password.component.scss'
 })
 export class RestorePasswordComponent {
-
+  @Input() email: string = ""
 }
