5fefbaeb357a8cfc70675317c62fe2596c6b6b38
Matheus Robusti Henriques Marqui <CIANDT\mmarqui@lnb024261cps.ciandt.global>
2024-07-17 15:39:59 -0300

    Telas de envio de email e codigo

--- src/app/pages/restore-password/restore-password.component.ts
+++ src/app/pages/restore-password/restore-password.component.ts
@@ -1,13 +1,34 @@
 import { Component, Input } from '@angular/core';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-restore-password',
   standalone: true,
-  imports: [PrimaryInputComponent],
+  imports: [PrimaryInputComponent, CommonModule],
   templateUrl: './restore-password.component.html',
   styleUrl: './restore-password.component.scss'
 })
 export class RestorePasswordComponent {
   @Input() email: string = ""
+  firstStep: boolean = true;
+  secondStep: boolean = false;
+
+  constructor(private router: Router) {}
+
+  sendEmail() {
+    this.firstStep = false;
+    this.secondStep = true;
+  }
+
+  back() {
+    if (!this.firstStep) {
+      this.firstStep = true;
+      this.secondStep = false;
+
+    } else {
+      this.router.navigate(['/login'])
+    }
+  }
 }
