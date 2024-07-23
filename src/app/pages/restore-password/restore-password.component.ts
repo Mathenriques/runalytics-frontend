78a3b3756e4966ee1f09c105de9e791d0b57c8a5
Matheus Robusti Henriques Marqui <CIANDT\mmarqui@lnb024261cps.ciandt.global>
2024-07-23 15:40:44 -0300

    Add forms no componente

--- src/app/pages/restore-password/restore-password.component.ts
+++ src/app/pages/restore-password/restore-password.component.ts
@@ -2,24 +2,53 @@ import { Component, Input } from '@angular/core';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { SendEmailService } from '../../services/api/send-email.service';
 
 @Component({
   selector: 'app-restore-password',
   standalone: true,
-  imports: [PrimaryInputComponent, CommonModule],
+  imports: [PrimaryInputComponent, CommonModule, ReactiveFormsModule],
+  providers: [
+    SendEmailService,
+    { provide: ToastrService, useClass: ToastrService },
+  ],
   templateUrl: './restore-password.component.html',
   styleUrl: './restore-password.component.scss'
 })
 export class RestorePasswordComponent {
-  @Input() email: string = ""
+
+  emailForm: FormGroup;
+  codeForm: FormGroup;
+
   firstStep: boolean = true;
   secondStep: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private sendEmailService: SendEmailService,
+    private toastService: ToastrService
+  ) {
+    this.emailForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
+    
+    this.codeForm = new FormGroup({
+      code: new FormControl('', [Validators.required])
+    });
+  }
 
   sendEmail() {
-    this.firstStep = false;
-    this.secondStep = true;
+    console.log(this.emailForm.value.email);
+    this.sendEmailService.execute(this.emailForm.value.email).subscribe({
+      next: () => {
+        this.firstStep = false;
+        this.secondStep = true;
+      },
+      error: () =>  this.toastService.error('Error ao enviar email, tente mais tarde') 
+    })
+    
   }
 
   back() {
